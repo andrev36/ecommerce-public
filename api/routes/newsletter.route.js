7b428a45a8880838d7e879bd6d81d437c2e32ad5
andrev36 <wiadernyandrzej96@gmail.com>
2020-12-23 16:36:31 +0100

    Public preview version

---
+++ api/routes/newsletter.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const newsletterController = require('../controllers/newsletter.controller')
+
+const newsletterRouter = express.Router()
+
+newsletterRouter
+ .route('/newsletter')
+ .post(newsletterController.sendNewsletterEmail)
+
+module.exports = newsletterRouter
